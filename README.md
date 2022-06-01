ce4f73e3eec4bcb8491805766d34142b3bb7c49d
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-06-01 17:06:33 +0000

    Added program

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# ICS2O-Unit#-##-HTML
+# ICS2O-Unit6-04-HTML
 
-[![GitHub's Super Linter](https://github.com/CristianoSellitto/<REPOSITORY>/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/CristianoSellitto/<REPOSITORY>/actions)
+[![GitHub's Super Linter](https://github.com/CristianoSellitto/ICS2O-Unit6-04-HTML/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/CristianoSellitto/ICS2O-Unit6-04-HTML/actions)
 
-[![Run on Repl.it](https://repl.it/badge/github/CristianoSellitto/<REPOSITORY>)](https://repl.it/github/CristianoSellitto/<REPOSITORY>)
+[![Run on Repl.it](https://repl.it/badge/github/CristianoSellitto/ICS2O-Unit6-04-HTML)](https://repl.it/github/CristianoSellitto/ICS2O-Unit6-04-HTML)
 
-This site can be found at: [https://CristianoSellitto.github.io/<REPOSITORY>](https://CristianoSellitto.github.io/<REPOSITORY>)
+This site can be found at: [https://CristianoSellitto.github.io/ICS2O-Unit6-04-HTML](https://CristianoSellitto.github.io/ICS2O-Unit6-04-HTML)
--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
 // Cristiano
-// ICS2O-Unit0-00-HTML
-// Date 2022
+// ICS2O-Unit6-04-HTML
+// June 1 2022
 
 'use strict'
 /**
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/<REPOSITORY>/sw.js", {
-    scope: "/<REPOSITORY>/",
+  navigator.serviceWorker.register("/ICS2O-Unit6-04-HTML/sw.js", {
+    scope: "/ICS2O-Unit6-04-HTML/",
   })
 }
 
 /**
- * Alerts "Hello, World!"
+ * Calculates the area of a trapezoid using the varibles in the URL on load.
  */
-function onButtonClick() {
-  alert("Hello, World!")
+window.onload = function() {
+  const params = new URLSearchParams(document.location.search)
+  const sideA = parseInt(params.get('a'))
+  const sideB = parseInt(params.get('b'))
+  const height = parseInt(params.get('h'))
+  var answer = ((sideA + sideB) / 2) * height
+
+  if (sideA && sideB && height) {
+    document.getElementById("varibles").innerHTML = "<h5>a = " + sideA + "</h5>" + "<h5>b = " + sideB + "</h5>" + "<h5>h = " + height + "</h5>"
+  } else {
+    document.getElementById("varibles").innerHTML = "<h5>a = null</h5>" + "<h5>b = null</h5>" + "<h5>h = null</h5>"
+    answer = 0
+  }
+  document.getElementById("answer").innerHTML = "<h4>The area of this trapezoid is " + answer + "cm².</h4>"
 }
